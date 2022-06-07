ce4671d1dffd97a55c935d18a8e806bf2dac78ee
zz98 <41350290+yisun98@users.noreply.github.com>
2022-06-07 11:39:15 +0800

    Add files via upload

---
+++ models/utils.py
@@ -0,0 +1,19 @@
+import os
+from math import ceil
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.autograd import Variable
+
+def initialize_weights(*models):
+    for model in models:
+        for module in model.modules():
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+                nn.init.kaiming_normal(module.weight)
+                if module.bias is not None:
+                    module.bias.data.zero_()
+            elif isinstance(module, nn.BatchNorm2d):
+                module.weight.data.fill_(1)
+                module.bias.data.zero_()
